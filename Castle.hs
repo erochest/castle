e8238bbeed363b751137b4319f88746b18019cae
Eric Rochester <erochest@virginia.edu>
2013-12-03 21:49:10 -0500

    Refactoring and reformatting.

--- Castle.hs
+++ Castle.hs
@@ -43,8 +43,8 @@ installCastle = do
           leaves = map (# [])
 
 getSandboxDir :: T.Text -> Sh FilePath
-getSandboxDir name =
-    liftIO $ fmap ((FS.</> S.fromText name) . (FS.</> "castles")) castleDir
+getSandboxDir name = liftIO $ fmap subdir castleDir
+    where subdir base = base FS.</> "castles" FS.</> S.fromText name
 
 withSandbox :: T.Text -> (FilePath -> Sh ()) -> (FilePath -> Sh ()) -> Sh ()
 withSandbox name onExists onFail = do
@@ -52,7 +52,7 @@ withSandbox name onExists onFail = do
     exists     <- test_d sandboxDir
     if exists
         then onExists sandboxDir
-        else onFail sandboxDir
+        else onFail   sandboxDir
 
 withSandbox' :: T.Text -> (FilePath -> Sh ()) -> Sh ()
 withSandbox' name onExists = withSandbox name onExists noop
@@ -71,14 +71,12 @@ listCastles =   liftIO (fmap (FS.</> "castles") castleDir)
 castle :: CastleCmd -> Sh ()
 castle ListCmd = mapM_ echo =<< listCastles
 
-castle NewCmd{..} = withSandbox
-    castleName
+castle NewCmd{..} = withSandbox castleName
     (const $ errorExit $ "Sandbox " <> castleName <> " already exists.")
     (\d -> mkdir_p d >> chdir d
             (sandbox_ "init" ["--sandbox=" <> toTextIgnore d]))
 
-castle UseCmd{..} = withSandbox
-    castleName
+castle UseCmd{..} = withSandbox castleName
     (\d -> pwd >>= cp (d FS.</> "cabal.sandbox.config"))
     (const $ errorExit $ "Sandbox " <> castleName <> " does not exist.\
                          \ Create it with 'sandbox new'.")
@@ -99,8 +97,7 @@ castle RemoveCmd = do
         errorExit "No sandbox in this directory."
     rm configFile
 
-castle DeleteCmd{..} = withSandbox
-    castleName
+castle DeleteCmd{..} = withSandbox castleName
     rm_rf
     (const $ errorExit $ "Sandbox " <> castleName <> " does not exist.")
 
@@ -156,8 +153,7 @@ main = do
 
 -- | This is a builder utility for ParserInfo instances.
 pinfo :: Parser a -> String -> InfoMod a -> ParserInfo a
-pinfo p desc imod = info (helper <*> p)
-                         (fullDesc <> progDesc desc <> imod)
+pinfo p desc imod = info (helper <*> p) (fullDesc <> progDesc desc <> imod)
 
 textOption :: Mod OptionFields T.Text -> Parser T.Text
 textOption fields = nullOption (reader (pure . T.pack) <> fields)
